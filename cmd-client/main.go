18700d412ed79e592c9b70eaf366406d39e879a5
kahono922 <jackson.kahono@student.moingaschool>
2022-09-04 19:32:24 +0300

    achieved functionality

--- cmd-client/main.go
+++ cmd-client/main.go
@@ -1,9 +1,9 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
-	"fmt"
 
 	cli "github.com/urfave/cli/v2"
 	"gorm.io/driver/sqlite"
@@ -12,33 +12,73 @@ import (
 
 type Record struct {
 	gorm.Model
-	Date string
+	Date    string
 	Comment string
 	Amount  float64
 }
 
 func main() {
 	app := &cli.App{
-		Flags: []cli.Flag{
-			&cli.StringFlag{
-				Name:    "lang",
-				Aliases: []string{"l"},
-				Value:   "english",
-				Usage:   "language for the greeting",
-				EnvVars: []string{"LEGACY_COMPAT_LANG", "APP_LANG", "LANG"},
+		Usage: "A simple expense tracker",
+
+		Commands: []*cli.Command{
+			{
+				Name:  "add",
+				Usage: "Add a record of your spending",
+				Flags: []cli.Flag{
+					&cli.StringFlag{
+						Name:     "date",
+						Aliases:  []string{"d"},
+						Usage:    "Date in format YYYY-MM-DD",
+						Required: true,
+					},
+					&cli.StringFlag{
+						Name:     "comment",
+						Aliases:  []string{"c"},
+						Usage:    "Comment (What did you spend money on?)",
+						Required: true,
+					},
+					&cli.Float64Flag{
+						Name:     "amount",
+						Aliases:  []string{"a"},
+						Usage:    "Amount (The amount of money you spent)",
+						Required: true,
+					},
+				},
+				Action: func(c *cli.Context) error {
+					r := Record{
+						Date:    c.String("date"),
+						Comment: c.String("comment"),
+						Amount:  c.Float64("amount"),
+					}
+					fmt.Println(addRecord(r))
+					return nil
+				},
+
 			},
-		},
-		Action: func(c *cli.Context) error {
-			name := "someone"
-			if c.NArg() > 0 {
-				name = c.Args().Get(0)
-			}
-			if c.String("lang") == "spanish" {
-				log.Printf("Hola %s!", name)
-			} else {
-				log.Printf("Hello %s!", name)
-			}
-			return nil
+			{
+				Name:  "get",
+				Usage: "Get records of your spending by date(optional)\nomit date to get all records",
+				Flags: []cli.Flag{
+					&cli.StringFlag{
+						Name:    "date",
+						Aliases: []string{"d"},
+						Usage:   "Date in format YYYY-MM-DD",
+					},
+
+				},
+				Action: func(c *cli.Context) error {
+					date := c.String("date")
+					if date == "" {
+						records := getAllRecords()
+						displayTable(records)
+					} else {
+						records := getRecordByDate(date)
+						displayTable(records)
+					}
+					return nil
+				},
+			},
 		},
 	}
 
@@ -52,33 +92,40 @@ func initDB() *gorm.DB {
 	if err != nil {
 		panic("failed to connect database")
 	}
+	db.AutoMigrate(&Record{})
 	return db
 }
 
-func addRecord(r Record) string{
+func addRecord(r Record) string {
 	db := initDB()
-	db.Create(&r)
+	if err:=db.Create(&r).Error;err!=nil{
+		return "Error adding record:"+err.Error()
+	}
 	return "Successfully added record"
 }
 
-func getAllRecords() []Record{
+func getAllRecords() []Record {
 	db := initDB()
 	var records []Record
-	db.Find(&records)
+	if err := db.Find(&records).Error; err != nil {
+		panic("failed to get records")
+	}
 	return records
 }
 
-func getRecordByDate(date string) []Record{
+func getRecordByDate(date string) []Record {
 	db := initDB()
 	var records []Record
-	db.Where("date = ?", date).Find(&records)
+	if err := db.Where("date = ?", date).Find(&records).Error; err != nil {
+		panic("failed to find record")
+	}
 	return records
 }
 
-func displayTable(records []Record){
+func displayTable(records []Record) {
 	fmt.Println("Date\t\tComment\t\tAmount")
 	for _, record := range records {
-		fmt.Println(record.Date, record.Comment, record.Amount)
+		//print as a table
+		fmt.Printf("%s\t%s\t\t%f\n", record.Date, record.Comment, record.Amount)
 	}
 }
-
